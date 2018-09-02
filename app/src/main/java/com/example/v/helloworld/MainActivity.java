fe917f432bfd70a8652bc6f3b4fcf3be0e344748
VikaMi <viktoria.mishchanyn@gmail.com>
2018-09-03 02:46:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/v/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.v.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Hello World"); //app
+        Log.d("myTag", "Hello World"); //console
+    }
+
+}
