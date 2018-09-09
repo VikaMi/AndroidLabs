18d2c24483f49649b46752b9573893a512f6e99c
VikaMi <viktoria.mishchanyn@gmail.com>
2018-09-09 23:28:44 +0300

    improved onCreate

--- app/src/main/java/com/example/v/helloworld/MainActivity.java
+++ app/src/main/java/com/example/v/helloworld/MainActivity.java
@@ -6,36 +6,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private TextView your_name;
-    private Button say_hello_button;
-    private Button clear_button;
-    private TextView hello_you;
-    private String yourName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OnButonsClick();
+    }
 
-        your_name = (EditText) findViewById(R.id.editText);
-        say_hello_button = findViewById(R.id.button2);
-        clear_button = findViewById(R.id.button);
-        hello_you = findViewById(R.id.textView);
+    public void OnButonsClick(){
+        final EditText your_name = (EditText) findViewById(R.id.editText);
+        Button say_hello_button = findViewById(R.id.button2);
+        Button clear_button = findViewById(R.id.button);
+        final TextView hello_you = findViewById(R.id.textView);
 
         say_hello_button.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        String yourName = your_name.getText().toString();
-                        hello_you.setText("Hello " + yourName);
+                        hello_you.setText(getString(R.string.say_hello, your_name.getText().
+                                toString().trim()));
                     }
-
                 });
+
         clear_button.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
